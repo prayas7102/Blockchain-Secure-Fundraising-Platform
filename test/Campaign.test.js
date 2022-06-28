8f631c3416e027ec97d8362ebfed5b7361ae1a28
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-06-28 06:54:44 +0530

    campaign contracts done

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -14,6 +14,9 @@ let factory;
 let campaignAddress;
 let campaign;
 
+//compiledFactory.abi is abi interface
+//compiledFactory.evm.bytecode.object is bytecode
+
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     factory = await new web3.eth.Contract((compiledFactory.abi))
@@ -23,6 +26,7 @@ beforeEach(async () => {
     await factory.methods.createCampaign('100').send({
         from: accounts[0], gas: '1000000'
     });
+    
 
     [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
     campaign = await new web3.eth.Contract((compiledCampaign.abi), campaignAddress);
@@ -33,4 +37,33 @@ describe('Campaigns', () => {
         assert.ok(factory._address);
         assert.ok(campaign._address);
     });
+    
+    it('marks caller as manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+    
+    it('allows people to contribute money and marks them as approvers', async() => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+        
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
+    });
+    
+    it('requires a minimum contribution', async()=>{
+        try{
+            await campaign.methods.contribute.send({
+                value:'5',
+                from: accounts[1]
+            });
+            
+            assert(false);
+        }
+        catch(err){
+            assert(err);
+        }
+    })
 })
