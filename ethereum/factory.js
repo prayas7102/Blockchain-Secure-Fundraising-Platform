aebe77224d4f0e66354347f01d756bbb44eb3544
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-07-12 19:27:19 +0530

    address of deployed contract updated

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -7,7 +7,7 @@ const Instance = new Contract(
     (compiledFactory.abi),
     "0x81E3bE14e1Dba985c46d899E905635a7fB4656Ee",
 );
-const res = await Instance.methods.getDeployedCampaigns().call();
-console.log(res);
+// const res = await Instance.methods.getDeployedCampaigns().call();
+// console.log(res);
 export default Instance;
 // use this command to run this file : node --experimental-json-modules factory.js
