4d9a524322b69d85418cb001ac97887f029dac15
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-07-13 18:15:37 +0530

    create campaign frontend

--- pages/campaign/new.js
+++ pages/campaign/new.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Layout from '../../components/Layout';
+import { Button, Checkbox, Form, Input } from 'semantic-ui-react';
+import Factory from '../../ethereum/factory';
 
 function New() {
+  const [minContribution, setMinContribution] = useState(0);
+  submitMinContribution = (event) => {
+    event.preventDefault();
+    await Factory.methods.createCampaign(minContribution)
+    // .send({from})
+  }
   return (
-    <div>New</div>
+    <Layout>
+      <h3>Create a Campaign</h3>
+      <Form onSubmit={submitMinContribution}>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <Input
+            label="wei"
+            labelPosition="right"
+            placeholder='Minimum Contribution'
+            value={minContribution}
+            onChange={(e) => {
+              setMinContribution(e.target.value)
+            }}
+          />
+        </Form.Field>
+        <Button type='submit' primary>Submit</Button>
+      </Form>
+    </Layout>
   )
 }
 
-export default New
+export default New;
