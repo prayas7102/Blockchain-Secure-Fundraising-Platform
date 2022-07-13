961560501c202d7957d79858a6700d4a90d97582
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-07-13 18:15:22 +0530

    layout prop children handling

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from './Header';
+import Head from 'next/head';
 import { Container } from 'semantic-ui-react';
 
-function Layout() {
+function Layout(props) {
     return (
         <Container>
+            <Head>
+                <link
+                    async
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+                />
+            </Head>
             <Header />
-            {Layout.children}
+            {props.children}
         </Container>
     )
 }
