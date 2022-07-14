173c87b173165ffea8055a47465059f09e3dbd3f
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-07-14 13:42:20 +0530

    express server created

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
+const app = next({ dev, hostname, port })
+
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+    const server = express();
+    server.get('*',(req,res)=>{
+        return handle(req,res);
+    })
+    server.listen(3000, (err) => {
+        if (err) throw err;
+        console.log('Server Ready');
+    })
+})
+.catch((err)=>{
+    console.log(err.message);
+})
